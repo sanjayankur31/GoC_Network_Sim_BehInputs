06b803af043b895f3500d2c0cd27ca3c93c9fed7
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2025-06-13 13:11:59 +0100

    feat: add util to print contents of pickle files

---
+++ NeuroML2/PythonUtils/print_pickle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Print contents of a Pickled file
+
+File: print_pickle.py
+
+Copyright 2025 Ankur Sinha
+Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
+"""
+
+import sys
+import pickle
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("One argument required: pickle file")
+        sys.exit(-1)
+
+    print(f"Printing contents of {sys.argv[1]}\n")
+    with open(sys.argv[1], 'rb') as f:
+        data = pickle.load(f, encoding="bytes")
+        print(data)
